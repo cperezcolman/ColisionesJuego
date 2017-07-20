356c60c50d2bbe238b38c8d07ab2fe22b5a0ca3c
Carlos Perez <cperez261@gmail.com>
2017-07-20 12:25:15 -0400

    prueba modificacion

--- src/main/Main.java
+++ src/main/Main.java
@@ -5,6 +5,8 @@ import javax.swing.*;
 public class Main {
 
     public static void main(String[] args) {
+
+        //cambio
         JFrame ventana = new JFrame("Colisiones Juego");
         ventana.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         ventana.setResizable(false);
